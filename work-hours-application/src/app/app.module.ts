2e19cc5d659f693753dae5981b360c26c19c5df7
Antti Karjalainen <antti.karjalainen@student.saimia.fi>
2020-02-25 23:29:36 +0200

    Created project

---
+++ work-hours-application/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MatToolbarModule
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
